f0f60ef1c585edf243d6414ba82e917318b51e55
wachira-cipher <118813864+wachira-cipher@users.noreply.github.com>
2022-12-13 10:50:29 -0800

    Add files via upload

---
+++ while_loop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE>
+<html>
+    <body>
+        <?php
+        $a = 5;
+        $num = 40;
+        while($i>5){
+            $i++;
+            $num--;
+        }
+        echo "$a and $num";
+        ?>
+
+    </body>
+</html>
