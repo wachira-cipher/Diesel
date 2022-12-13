76a3d95d4e116685b30b0e08ab846aaf1bf16c37
wachira-cipher <118813864+wachira-cipher@users.noreply.github.com>
2022-12-13 10:38:20 -0800

    Add files via upload

---
+++ if_else statement.php
@@ -0,0 +1,12 @@
+<!DOCTYPE>
+<html>
+    <body>
+        <?php
+    $m = $marks("M");
+        if ($m == "40")
+            echo "you passed exams";
+        else
+            echo "you failed";
+    ?>
+    </body>
+</html>
