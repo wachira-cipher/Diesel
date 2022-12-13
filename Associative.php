679d382cab8a6d8cddf5e61ec6ea11a53217a25f
wachira-cipher <118813864+wachira-cipher@users.noreply.github.com>
2022-12-13 11:48:02 -0800

    Add files via upload

---
+++ Associative.php
@@ -0,0 +1,11 @@
+<!DOCTYPE >
+<html>
+    <body>
+        <?php
+        $salaries = array("jonathan" => 30, 000, "wachira" => 20, 000, "tyson" => 10, 000, );
+        echo "salary for jonathan is" . $salaries['jonathan'] . "<br/>";
+        echo "salary for wachira is" . $salaries['wachira'] . "<br/>";
+        echo "salary for tyson is" . $salaries['tyson'] . "<br/>";
+        ?>
+    </body>
+</html>
